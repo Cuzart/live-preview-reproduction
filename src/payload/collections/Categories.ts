2a0634b52ad05a8575713b2f7630cbd8db424df8
Chris Heinz <chrisi.heinz@web.de>
2024-01-17 08:46:20 +0100

    add demo for local api update nested fields

--- src/payload/collections/Categories.ts
+++ src/payload/collections/Categories.ts
@@ -1,3 +1,4 @@
+import payload from 'payload'
 import type { CollectionConfig } from 'payload/types'
 
 const Categories: CollectionConfig = {
@@ -14,6 +15,30 @@ const Categories: CollectionConfig = {
       type: 'text',
     },
   ],
+  hooks: {
+    afterChange: [
+      async ({ doc, operation }) => {
+        if (operation === 'create') {
+          const { docs } = await payload.find({
+            collection: 'pages',
+            where: { slug: { equals: 'home' } },
+            depth: 10,
+          })
+
+          const req = await payload.update({
+            id: docs[0].id,
+            collection: 'pages',
+            data: {
+              layout: docs[0].layout,
+            },
+            depth: 10,
+          })
+
+          console.log(req)
+        }
+      },
+    ],
+  },
 }
 
 export default Categories
